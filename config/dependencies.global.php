e0aef78568d9b31b92d4d60b20b6fcb39f956956
Andres Galindo <andres.galindo@flexshopper.com>
2016-01-29 08:40:23 -0500

    added validators to dep file

--- config/dependencies.global.php
+++ config/dependencies.global.php
@@ -2,6 +2,10 @@
 
 use StdLib\Console\ApplicationFactory;
 use StdLib\Console\Application;
+use StdLib\Validator\ValidationMiddleware;
+use StdLib\Validator\ValidationMiddlewareFactory;
+use StdLib\Validator\Validator;
+use StdLib\Validator\ValidatorFactory;
 use \StdLib\Worker\DirectedManagerAbstractFactory;
 use \StdLib\Worker\DirectedWorkerAbstractFactory;
 
@@ -12,7 +16,9 @@ return [
             DirectedWorkerAbstractFactory::class,
         ],
         'factories' => [
-            Application::class => ApplicationFactory::class,
+            Application::class          => ApplicationFactory::class,
+            Validator::class            => ValidatorFactory::class,
+            ValidationMiddleware::class => ValidationMiddlewareFactory::class,
         ]
     ]
 ];
